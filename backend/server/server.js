ed12280a370f1103ebc528e1512bd0fc23a85abb
Alex Kalts <alexkalts6@gmail.com>
2020-06-13 18:32:49 +0300

    "predict" page ready, confirm it works

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-const naivebayes = require('ml-naivebayes');
+const cart = require('ml-cart');
 
 const {Metric} = require('./models/metric');
 
@@ -60,10 +60,10 @@ app.get('/train', (req, res) => {
         labels_agegroup = [];
         labels_gender = [];
         for(var i=0, l=metrics.length; i < l; i++){
-            point = [];
+            var point = [];
             point.push(metrics[i].mouseSpeedAvg);
             point.push(metrics[i].mouseSpeedMin);
-            point.push(metrics[i].mousespeedMax);
+            point.push(metrics[i].mouseSpeedMax);
             point.push(metrics[i].nofKeysPressed);
             point.push(metrics[i].nofMouseClicks);
             point.push(metrics[i].nofMouseMoves);
@@ -73,7 +73,7 @@ app.get('/train', (req, res) => {
             point.push(metrics[i].timeBetweenClicksMax);
             point.push(metrics[i].timeBetweenClicksMin);
             point.push(metrics[i].timeBetweenKeysAvg);
-            point.push(metrics[i].timeBetweenClicksMax);
+            point.push(metrics[i].timeBetweenKeysMax);
             point.push(metrics[i].timeBetweenKeysMin);
             point.push(metrics[i].typingErrors);
             point.push(metrics[i].typingTime);
@@ -85,18 +85,58 @@ app.get('/train', (req, res) => {
         agegroups = ['underaged', 'young', 'adult', 'elderly'];
         labels_gender = labels_gender.map((elem) => genders.indexOf(elem));
         labels_agegroup = labels_agegroup.map((elem) => agegroups.indexOf(elem));
-        
+
         // train the classifiers
-        agegroup_classifier = new naivebayes.MultinomialNB();
-        gender_classifier = new naivebayes.MultinomialNB();
+        var options = { gainFunction: 'gini', maxDepth: 20, minNumSamples:1};
+
+        agegroup_classifier = new cart.DecisionTreeClassifier(options);
+        gender_classifier = new cart.DecisionTreeClassifier(options);
 
         agegroup_classifier.train(data, labels_agegroup);
         gender_classifier.train(data, labels_gender);
+
+        console.log("Training successful!");
     },
     (error) => {
         res.status(400).send(error);
     })
 });
+
+
+// predict
+app.post('/predict', (req, res) => {
+    var point = [];
+    point.push(req.body.mouseSpeedAvg);
+    point.push(req.body.mouseSpeedMin);
+    point.push(req.body.mouseSpeedMax);
+    point.push(req.body.nofKeysPressed);
+    point.push(req.body.nofMouseClicks);
+    point.push(req.body.nofMouseMoves);
+    point.push(req.body.puzzleAvgMoveTime);
+    point.push(req.body.puzzleMoves);
+    point.push(req.body.timeBetweenClicksAvg);
+    point.push(req.body.timeBetweenClicksMax);
+    point.push(req.body.timeBetweenClicksMin);
+    point.push(req.body.timeBetweenKeysAvg);
+    point.push(req.body.timeBetweenKeysMax);
+    point.push(req.body.timeBetweenKeysMin);
+    point.push(req.body.typingErrors);
+    point.push(req.body.typingTime);
+
+    var pred_age = agegroup_classifier.predict([point]);
+    var pred_gender = gender_classifier.predict([point]);
+
+    var genders = ['female', 'male'];
+    var agegroups = ['underaged', 'young', 'adult', 'elderly'];
+
+    var predicted_agegroup = agegroups[pred_age[0]];
+    var predicted_gender = genders[pred_gender[0]];
+    
+    res.status(200).send({
+        agegroup: predicted_agegroup, gender: predicted_gender
+    });
+});
+
 
 // create new
 app.post('/metrics', (req, res) => {
